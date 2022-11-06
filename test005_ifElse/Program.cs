8f33297c793e40072da9b1f26b73fcc18001f393
Sergei_Lanikhin <sergei.lanikhin@gmail.com>
2022-11-06 22:37:27 +0300

    added test005_iflse file

---
+++ test005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "racoon")
+{
+    Console.WriteLine("WOW, it's Grait Racoon!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
