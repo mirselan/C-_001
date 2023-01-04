572270dc5b3722bc7a89480e31e708d65b5c185f
Sergei_Lanikhin <sergei.lanikhin@gmail.com>
2023-01-05 00:13:40 +0300

    test009_array_methods added

---
+++ test009_array_methods/Program.cs
@@ -0,0 +1,22 @@
+﻿void FillArray (int[] collection) {
+    int length = collection.Length;
+    int count = 0;
+    while (count < length) {
+        collection[count] = new Random().Next(1, 10);
+        count++;
+    }
+}
+
+void PrintArray(int[] col) {
+    int len = col.Length;
+    int idx = 0;
+    while (idx < len) {
+        Console.WriteLine(col[idx]);
+        idx++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
