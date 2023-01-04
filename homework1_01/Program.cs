5650b2fc912df3ce5be35345c911c55afc3ec70d
Sergei_Lanikhin <sergei.lanikhin@gmail.com>
2023-01-04 18:43:11 +0300

    homework tasks 1, 2, 3, 4 for lesson 1 added

---
+++ homework1_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Enter integer number a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter integer number b: "); 
+int b = int.Parse(Console.ReadLine());
+int max = a;
+
+if (a > max) {
+    max = a;
+}
+else {
+    max = b;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
---
+++ homework1_04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+//
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Enter integer number: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= N) {
+    if (count % 2 == 0) {
+        if (N - count == 1 || N == count) {
+            Console.Write(count);
+        }
+        else {
+            Console.Write(count);
+            Console.Write(",");
+            Console.Write(" ");
+        }
+        
+    }
+    count++;
+}
