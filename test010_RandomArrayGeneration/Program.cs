23a126c91a923cfc475b3706e293b48925c234e9
Sergei_Lanikhin <sergei.lanikhin@gmail.com>
2023-01-06 00:12:43 +0300

    random array generator and editor added

---
+++ test010_RandomArrayGeneration/Program.cs
@@ -0,0 +1,19 @@
+﻿// Random array generation with arguments and displaying result:
+////////////////////////////////////////////////////////////////
+int[] RandomArrayGen (int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void ShowArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] arr = RandomArrayGen(8, 0, 1);
+ShowArray(arr);
