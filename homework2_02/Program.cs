23da64bdc25de306a024cb34ba75394fc66af0ac
Sergei_Lanikhin <sergei.lanikhin@gmail.com>
2023-01-05 15:10:41 +0300

    homework for lesson 2 added

---
+++ homework2_02/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+///////////////////////////////////////////////////////////////////////////////
+int MinusToPlus (int number) {
+    return number * -1;
+}
+
+string ThirdDigit (int num) {
+    if (num < 0) num = MinusToPlus(num);
+    string numStr = num.ToString();
+    if (numStr.Length >= 3) {
+        return numStr[2].ToString();
+    }
+    else {
+        return "третьей цифры нет";
+    }
+}
+
+Console.Write("Enter your number: ");
+Console.WriteLine(ThirdDigit(Convert.ToInt32(Console.ReadLine())));
---
+++ homework2_03/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру,
+ обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
+//////////////////////////////////////////////////////////////////////////
+string DayOfWeek (int day) {
+    if (day < 6) return "нет";
+    else return "да";
+}
+
+Console.Write("Enter number of the day from 1 to 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day >= 1 && day < 8) Console.WriteLine(DayOfWeek(day));
+else Console.Write("There are just 7 days in week.");
