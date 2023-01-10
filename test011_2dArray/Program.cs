4f4852d0fcf7ee143028a8374d30b5cb69eab2ca
Sergei_Lanikhin <sergei.lanikhin@gmail.com>
2023-01-10 12:03:59 +0300

    test011_2darray added

---
+++ test011_2dArray/Program.cs
@@ -0,0 +1,24 @@
+﻿void Show2dArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }    
+}
+
+void FillRandom2dArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+int[,] array = new int[4, 5];
+Show2dArray(array);
+Console.WriteLine();
+FillRandom2dArray(array);
+Show2dArray(array);
