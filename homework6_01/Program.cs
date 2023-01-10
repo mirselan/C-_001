7b86292a8fa1affaf9b415cc0b749e4b256941a5
Sergei_Lanikhin <sergei.lanikhin@gmail.com>
2023-01-10 16:16:59 +0300

    homework for lesson 6 added, task 02 is not solved yet

---
+++ homework6_01/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+////////////////////////////////////////////////////////
+int[] UserChoiceArrayGen (int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter {i + 1} element of array with integer digits: ");
+        int el = Convert.ToInt32(Console.ReadLine());
+        arr[i] = el;
+    }
+    return arr;
+}
+
+void ShowArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int GreaterThenZeroNum (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) count++;
+    return count;
+}
+
+Console.Write("Enter how much numbers do want to input: ");
+int nums = Convert.ToInt32(Console.ReadLine());
+int[] userArray = UserChoiceArrayGen(nums);
+ShowArray(userArray);
+Console.WriteLine();
+int result = GreaterThenZeroNum(userArray);
+Console.WriteLine(result);
---
+++ homework6_02/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 43: Напишите программу, которая найдёт 
+точку пересечения двух прямых, заданных уравнениями
+ y = k1 * x + b1, y = k2 * x + b2; 
+ значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) */
+//////////////////////////////////////////////////////
+double[] UserChoiceArrayGen ()
+{
+    double[] arr = new double[4];
+    string[] arrKB = {"b1", "k1", "b2", "k2"};
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Enter {arrKB[i]}: ");
+        double el = Convert.ToDouble(Console.ReadLine());
+        arr[i] = el;
+    }
+    return arr;
+}
+
+void ShowArray (double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+// string FindPoint (double[] array)
+// {
+//     double x;
+//     double y = array[1] * x +array[0];
+//     double 
+// }
+
+double[] koefs =  UserChoiceArrayGen();
+ShowArray(koefs);
+
